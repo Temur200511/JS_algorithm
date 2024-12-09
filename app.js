864112befc3449456562fe37b35c5a4ccb8d4a26
Temur200511 <tziyodullayev61@gmail.com>
2024-12-09 10:11:05 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+// const n = parseInt(prompt("n ni kiriting (n > 0):"));
+// let S = 0;
+// for (let i = 1; i <= n; i++) {
+//     S += Math.pow(-1, i + 1) * (1 / i);
+// }
+// console.log(S);
+
+
+
+
+// function faktorialYigindi(n) {
+//     let umumiy = 0;
+//     let fakultet = 1;
+//     let i = 1;
+//     while (i <= n) {
+//       fakultet *= i;
+//       umumiy += fakultet;
+//       i++;
+//     }
+//     return umumiy;
+//   }
+  
+//   let n = parseInt(prompt("Musbat butun son kiriting (n > 0):"));
+//   alert("Faktorial yig'indisi: " + faktorialYigindi(n));
+  
+
+
+
+
+
+
+// function engKattaK(n) {
+//     let k = 0;
+//     while ((k + 1) ** 2 <= n) {
+//       k++;
+//     }
+//     return k;
+//   }
+  
+//   let n = parseInt(prompt("Musbat natural son kiriting (n > 0):"));
+//   alert("k^2 <= n shartiga mos keladigan eng katta k: " + engKattaK(n));
+  
+
+
+
+
+
+
+// function engKichikK(n) {
+//     let k = 0;
+//     while (k ** 2 <= n) {
+//       k++;
+//     }
+//     return k;
+//   }
+  
+//   let n = parseInt(prompt("Musbat natural son kiriting (n > 0):"));
+//   alert("k^2 > n shartiga mos keladigan eng kichik k: " + engKichikK(n));
+  
